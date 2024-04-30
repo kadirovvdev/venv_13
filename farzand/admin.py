f7ab16999ba82a3a59f21569129a972a59d846e6
Jahongir <jahongirq702@gmail.com>
2024-05-01 03:08:50 +0500

    dw

---
+++ farzand/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Father
+
+admin.site.register(Father)
+
+# Register your models here.
---
+++ farzand/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FarzandConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'farzand'
